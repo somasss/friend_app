360877e27a708ab0d2cc4470244d16bbe157e7c8
Yosuke Soma <ssyy20210101@gmail.com>
2022-04-25 07:24:37 +0900

    blogコントローラ/モデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :feeds
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :blogs
 end
