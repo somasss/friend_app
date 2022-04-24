eb4731d0e548f5a64fdee0798267fcddcf661151
Yosuke Soma <ssyy20210101@gmail.com>
2022-04-25 05:40:05 +0900

    login_requiredメソッドの追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
   end
 
@@ -17,6 +19,6 @@ class SessionsController < ApplicationController
     session.delete(:user_id)
     flash[:notice] = 'ログアウトしました'
     redirect_to new_session_path
-  endj
+  end
 
 end
