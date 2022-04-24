a4148560b05f7a488e0d123b67b2d23db4932456
Yosuke Soma <ssyy20210101@gmail.com>
2022-04-25 08:33:52 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   before_validation { email.downcase! }
+  has_many :feeds
   validates :password, length: { minimum: 6 }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_24_221420) do
+ActiveRecord::Schema.define(version: 2022_04_24_232713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2022_04_24_221420) do
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema.define(version: 2022_04_24_221420) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "feeds", "users"
 end
